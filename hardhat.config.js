fed2f159cb121deadf3bed231ae717354f33408e
sknek <sknek@ya.ru>
2022-08-23 18:01:58 +0300

    -

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,16 +1,17 @@
 require("@nomiclabs/hardhat-waffle");
 require('dotenv').config()
 
+const DEFAULT_COMPILER = process.env.DEFAULT_COMPILER || "0.8.7"
 const ALCHEMY_API_KEY = process.env.ALCHEMY_API_KEY
 const ROPSTEN_PRIVATE_KEY = process.env.ROPSTEN_PRIVATE_KEY
 const GOERLI_PRIVATE_KEY = process.env.GOERLI_PRIVATE_KEY
 const INFURA_API_KEY = process.env.INFURA_API_KEY
 const MOONRIVER_PRIVATE_KEY = process.env.MOONRIVER_PRIVATE_KEY
 
-const GOERLI_INFURA_URL = process.env.GOERLI_INFURA_URL || `https://goerli.infura.io/v3/${INFURA_API_KEY}`
-const ROPSTEN_INFURA_URL = process.env.ROPSTEN_INFURA_URL || `https://ropsten.infura.io/v3/${INFURA_API_KEY}`
-const KVOAN_ALCHEMY_URL = process.env.KVOAN_ALCHEMY_URL || `https://eth-kovan.alchemyapi.io/v2/${ALCHEMY_API_KEY}`
-const RINKBEY_ALCHEMY_URL = process.env.RINKBEY_ALCHEMY_URL || `https://eth-rinkeby.alchemyapi.io/v2/${ALCHEMY_API_KEY}`
+const GOERLI_URL = process.env.GOERLI_URL || `https://goerli.infura.io/v3/${INFURA_API_KEY}`
+const ROPSTEN_URL = process.env.ROPSTEN_URL || `https://ropsten.infura.io/v3/${INFURA_API_KEY}`
+const KVOAN_URL = process.env.KVOAN_URL || `https://eth-kovan.alchemyapi.io/v2/${ALCHEMY_API_KEY}`
+const RINKBEY_URL = process.env.RINKBEY_URL || `https://eth-rinkeby.alchemyapi.io/v2/${ALCHEMY_API_KEY}`
 const MOONRIVER_URL = process.env.MOONRIVER_URL || `https://rpc.moonriver.moonbeam.network/`
 
 /**
@@ -20,7 +21,7 @@ module.exports = {
   solidity: {
     compilers: [
       {
-        version: "0.8.7"
+        version: DEFAULT_COMPILER
       }
     ]
   },
@@ -30,7 +31,7 @@ module.exports = {
 
 if (GOERLI_PRIVATE_KEY) {
   module.exports.networks.goerli = {
-    url: GOERLI_INFURA_URL,
+    url: GOERLI_URL,
     accounts: [
       GOERLI_PRIVATE_KEY
     ]
@@ -39,14 +40,14 @@ if (GOERLI_PRIVATE_KEY) {
 
 if (ROPSTEN_PRIVATE_KEY) {
   module.exports.networks.ropsten = {
-    url: ROPSTEN_INFURA_URL,
+    url: ROPSTEN_URL,
     accounts: [
       ROPSTEN_PRIVATE_KEY
     ]
   }
 }
 
-if (ROPSTEN_PRIVATE_KEY) {
+if (MOONRIVER_PRIVATE_KEY) {
   module.exports.networks.moonriver = {
     url: MOONRIVER_URL,
     accounts: [
